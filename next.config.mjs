dc274294e8ffa8e1afcdc4061276feeda8359faf
Michael Baek <mbaek@paypal.com>
2024-06-08 22:52:14 -0400

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  outpot: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
